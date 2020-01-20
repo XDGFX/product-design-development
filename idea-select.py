946de923b11c10fa5550ffe13d3b5c4d6b5296a3
XDGFX <callum.morrison@mac.com>
2020-01-20 14:51:09 +0000

    added colours to the app

--- idea-select.py
+++ idea-select.py
@@ -3,11 +3,25 @@ import random
 import subprocess
 import re
 
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 subject = ' PRODUCT DESIGN & DEVELOPMENT '
 
 _, width = subprocess.check_output(['stty', 'size']).decode().split()
 width = int(width)
 title = subject.center(width, "-")
+
+title = title.replace(subject, bcolors.HEADER + subject + bcolors.ENDC)
 
 
 with open('_questions.txt', 'r') as q:
@@ -33,13 +47,13 @@ while 1:
 
     lines = []
     while True:
-        line = input("> ")
+        line = input(bcolors.OKBLUE + "> " + bcolors.ENDC)
         if line:
             lines.append(line)
         else:
             lines.append(line)
-            print("! Press enter once more to exit")
-            line = input("> ")
+            # print("! Press enter once more to exit")
+            line = input(bcolors.WARNING + "! " + bcolors.ENDC)
             if line:
                 lines.append(line)
             else:
@@ -48,16 +62,19 @@ while 1:
 
     os.system('clear')
 
-    print(" QUESTION ".center(width, "-"))
+    print(" QUESTION ".center(width, "-").replace("QUESTION",
+                                                  bcolors.HEADER + "QUESTION" + bcolors.ENDC))
     print("")
     print(questions[selected])
     print("")
 
-    print(" YOUR ANSWER ".center(width, "-"))
+    print(" YOUR ANSWER ".center(width, "-").replace("YOUR ANSWER",
+                                                     bcolors.HEADER + "YOUR ANSWER" + bcolors.ENDC))
     print("")
     print(text)
 
-    print(" NOTES ".center(width, "-"))
+    print(" NOTES ".center(width, "-").replace("NOTES",
+                                               bcolors.HEADER + "NOTES" + bcolors.ENDC))
     print("")
 
     for _, _, files in os.walk("."):
@@ -76,4 +93,4 @@ while 1:
     print("")
     print("-" * width)
     print("")
-    input("Press enter to continue... ")
+    input(bcolors.OKGREEN + "Press enter to continue... " + bcolors.ENDC)
